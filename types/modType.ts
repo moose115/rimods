c2ffe6f1cacc4a173e3ac67736f5c8fc47fdb235
Mustafa Ucuncu <mustafaucuncu@onet.pl>
2020-10-24 09:24:27 +0200

    Add mod type

---
+++ types/modType.ts
@@ -0,0 +1,11 @@
+type ModType = {
+  id: string;
+  name: string;
+  images: string[];
+  description: string;
+  tags: string[];
+  workshopLink: string;
+  ludeonLink: string;
+};
+
+export default ModType;
